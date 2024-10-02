4307c1197986b9c0781688779237399bda9df7ee
BEHeder <bryaneheder@gmail.com>
2024-10-02 14:05:47 -0700

    started components and databinding tutorial

---
+++ WDD_430/05-components-databinding/cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css'
+})
+export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  newServerContent = '';
+
+
+  onAddServer(nameInput: HTMLInputElement) {
+    // console.log(nameInput.value);
+    this.serverCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.newServerContent
+    });
+  }
+
+}
