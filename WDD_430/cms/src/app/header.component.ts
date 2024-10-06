58608cad27d2cb97a071f876caf2d937fe770a98
BEHeder <bryaneheder@gmail.com>
2024-10-05 22:55:45 -0700

    more or less completed week02 assignment

---
+++ WDD_430/cms/src/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+}
